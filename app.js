f077aa1817367bd2f10fe5490acae97ed1c4fb8a
0x.Balance <1137774080@qq.com>
2023-06-13 17:27:58 +0800

    test:test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  * @Author       : 
  * @Date         : 2023-05-29 19:28:08
  * @LastEditors  : Please set LastEditors
- * @LastEditTime : 2023-06-13 16:04:35
+ * @LastEditTime : 2023-06-13 16:58:06
  */
 import dotenv from 'dotenv'
 import Sequelize from 'sequelize';
@@ -33,3 +33,10 @@ const zks_startBlock = await getStartBlock(BlockConfigModel, 'zks_dev') || await
 const zksTestPool = new PoolSerice('wss://testnet.era.zksync.dev/ws', PairlistModel, BlockConfigModel, EzswapPoolModel, zks_startBlock.startBlock, '0xBcB7032c1e1Ea0Abc3850590349560e1333d6848', '0x8fB6a250adA61cDEA897C35f5404d94ada89633f', 'zks_dev')
 zksTestPool.start()
 zksTestPool.updatePairInfo()
+zksTestPool.provider._websocket.on("error", async (error) => {
+  this.provider._websocket.terminate();
+  setTimeout(() => {
+    zksTestPool.start()
+    zksTestPool.updatePairInfo()
+  }, 3000);
+});
