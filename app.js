3c5a921e8c59af0187660e53633de1211585d624
0x.Balance <1137774080@qq.com>
2023-06-14 15:52:58 +0800

    feat:change pooldata address

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  * @Author       : 
  * @Date         : 2023-05-29 19:28:08
  * @LastEditors  : Please set LastEditors
- * @LastEditTime : 2023-06-13 17:00:31
+ * @LastEditTime : 2023-06-14 15:52:15
  */
 import dotenv from 'dotenv'
 import Sequelize from 'sequelize';
@@ -30,7 +30,7 @@ await createModel(PairlistModel)
 await createModel(BlockConfigModel)
 
 const zks_startBlock = await getStartBlock(BlockConfigModel, 'zks_dev') || await insertStartBlock(BlockConfigModel, 6639000, 'zks_dev')
-const zksTestPool = new PoolSerice('wss://testnet.era.zksync.dev/ws', PairlistModel, BlockConfigModel, EzswapPoolModel, zks_startBlock.startBlock, '0xBcB7032c1e1Ea0Abc3850590349560e1333d6848', '0x8fB6a250adA61cDEA897C35f5404d94ada89633f', 'zks_dev')
+const zksTestPool = new PoolSerice('wss://testnet.era.zksync.dev/ws', PairlistModel, BlockConfigModel, EzswapPoolModel, zks_startBlock.startBlock, '0xBcB7032c1e1Ea0Abc3850590349560e1333d6848', '0x89484602989dF8eb6BB2AbEEAb977a910b39963b', 'zks_dev')
 zksTestPool.start()
 zksTestPool.updatePairInfo()
 zksTestPool.provider._websocket.on("error", async (error) => {
