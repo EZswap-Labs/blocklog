9a30611afcf9268874f16ebbf2a58720c6a5468f
zerooooooo1 <xushiduo1@gmail.com>
2024-01-09 11:24:58 +0800

    diandian增加eos脚本

--- app-eos-nft.js
+++ app-eos-nft.js
@@ -1,4 +1,4 @@
-import {ethers} from 'ethers'
+import {ethers, utils} from 'ethers'
 import ContractABI from "./abi/721seaport.js";
 import tokenAddress from "./db/models/tokenIdAddress.js";
 import Sequelize from "sequelize";
@@ -12,35 +12,35 @@ const iface = new ethers.utils.Interface(ContractABI);
 let provider = new ethers.providers.JsonRpcProvider("https://api.evm.eosnetwork.com/");
 
 // 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef maybe mint event
-let startBlock = 24044003
-async function syncBlock () {
+let startBlock = 24053903
+
+async function syncBlock() {
     // 实时获取最新区块
-    let endBlock = 24105038
+    let endBlock = await provider.getBlockNumber()
     while (startBlock <= endBlock) {
-        const currentEndBlock = startBlock + 100
+        const currentEndBlock = startBlock + 500
         console.log(startBlock, currentEndBlock)
         const filter = {
             address: "0xC10A988680355BdFfE0B998Cd12098264C3957Bd",
             fromBlock: startBlock,
-            endBlock: currentEndBlock,
+            toBlock: currentEndBlock,
             topics: [["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"]]
         };
-        console.log(1)
-        const logs=await provider.getLogs(filter);
-        console.log(2)
+        const logs = await provider.getLogs(filter);
         const tokenMap = new Map();
         for (const log of logs) {
-            // todo mint了又转移要去掉
-            // console.log(log.topics[1], log.topics[2], log.topics[3])
-            const a={
-                token_id: log.topics[3],
-                    address: log.topics[2]
+            // todo mint了又转移要去掉,如果topic[1]不是 0x0000000000000000000000000000000000000000000000000000000000000000, 要删掉数据库里,但是好像不用管,
+            //  因为有这条记录就直接覆盖之前的了
+            let normalizedAddress = '0x' + log.topics[2].slice(26);
+            const a = {
+                token_id: parseInt(log.topics[3], 16),
+                address: normalizedAddress
             }
-            tokenMap.set(log.topics[3],a)
+            tokenMap.set(parseInt(log.topics[3], 16), a)
         }
         if (tokenMap.size > 0) {
             const alsit = []
-            tokenMap.forEach(function(value, key) {
+            tokenMap.forEach(function (value, key) {
                 alsit.push(value)
             })
             await batchInsertToken(sqlConnect, alsit)
@@ -49,10 +49,10 @@ async function syncBlock () {
     }
 }
 
-async function main () {
+async function main() {
     await syncBlock()
     setTimeout(() => {
-         syncBlock()
+        syncBlock()
     }, 1000);
 }
 
